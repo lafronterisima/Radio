f26439b836a4db03df426d6688516469d6017b60
lafronterisima <159441596+lafronterisima@users.noreply.github.com>
2024-03-09 16:48:32 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
 $usuario  = "LA138US00001";
 $password = "Nata2024*";
-$servidor = "localhost";
-$puerto = 3306; 
+$servidor = "localhost:3306";
 $basededatos = "artistas";
-$con = mysqli_connect($servidor, $usuario, $password, $basededatos, $puerto) or die("No se ha podido conectar al Servidor");
+$con = mysqli_connect($servidor, $usuario, $password, $basededatos) or die("No se ha podido conectar al Servidor");
 ?>

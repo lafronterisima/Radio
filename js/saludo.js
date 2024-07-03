cf83a425d10b808300fe4a4b83fcfd84bcfef92b
lafronterisima <159441596+lafronterisima@users.noreply.github.com>
2024-07-03 15:22:46 -0500

    Add files via upload

--- js/saludo.js
+++ js/saludo.js
@@ -1,35 +1,18 @@
+ 
+function saludo(){
+    var fecha = new Date(); 
+    var hora = fecha.getHours();
+    
+    if (hora >= 0 && hora < 8) {
+        img.src = "imagenes/dia.png";
+    } else if (hora >= 8 && hora < 12) {
+        img.src = "imagenes/tiempo.png";
+    } else if (hora >= 12 && hora < 18) {
+        img.src = "imagenes/tarde.png";
+    } else if (hora >= 18 && hora < 24) {
+        img.src = "imagenes/noche.png";
+    }  
+}
 
-       function saludo(){
-   
-          fecha = new Date(); 
-          hora = fecha.getHours();
-
-           if(hora >= 0 && hora < 8){
-            pSaludo.textContent = "";
-			img.src="imagenes/dia.png";
-          }
-          
-  
-           if(hora >= 8 && hora < 12){
-            pSaludo.textContent = "";
-			img.src="imagenes/tiempo.png";
-          }      
-            
-		   if(hora >= 12 && hora < 18){
-            pSaludo.textContent = "";
-		    img.src="imagenes/tarde.png";
-          }
-		  
-          if(hora >= 18 && hora < 24){
-             pSaludo.textContent = "";
-		     img.src="imagenes/noche.png"; 
-          }	  
-       }
-
-   var img = document.getElementById("tiempo");
-   var pSaludo = document.getElementById("txtsaludo");
-   setInterval(saludo,100);
-   
-
-  
-  
+var img = document.getElementById("tiempo");
+setInterval(saludo);

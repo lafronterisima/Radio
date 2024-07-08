8b7af9b035e04131d2ec2cdbb6f72e4d1f918e41
lafronterisima <159441596+lafronterisima@users.noreply.github.com>
2024-07-08 05:03:46 -0500

    Add files via upload

--- js/pulsar.js
+++ js/pulsar.js
@@ -1,61 +1,43 @@
-
-
-$(document).ready(function(){
-    
-    $("#pulsar1").click(function(){
-        $("#contex").load("sub/news1.html");
-     
-    });
-    
-    $("#pulsar2").click(function(){
-        $("#contex").load("sub/news2.html");
-       
-    });
-    
-    $("#pulsar3").click(function(){
-        $("#contex").load("sub/news3.html");
-       
-    });
-    
-    $("#pulsar4").click(function(){
-        $("#contex").load("sub/news4.html");
-      
-    });
-    
-     $("#pulsar5").click(function(){
-        $("#contex").load("sub/news5.html");
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 1; i <= 10; i++) {D
+        let pulsar = document.getElementById("pulsar" + i);
         
-    });
-    
-     $("#pulsar6").click(function(){
-        $("#contex").load("sub/news6.html");
-       
-    });
-    
-     $("#pulsar7").click(function(){
-        $("#contex").load("sub/news7.html");
-       
-    });
-    
-     $("#pulsar8").click(function(){
-        $("#contex").load("sub/news8.html");
-        
-    });
-    
-     $("#pulsar9").click(function(){
-        $("#contex").load("sub/news9.html");
-       
-    });
-    
-     $("#pulsar10").click(function(){
-        $("#contex").load("sub/news10.html");
-        
-    });
-                  
+  
+        pulsar.addEventListener("click", function() {
+            loadContent("sub/news" + i + ".html");
+        });
+    }
 });
+
+function loadContent(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            document.getElementById("contex").innerHTML = xhr.responseText;
+        }
+    };
+    
+    xhr.send();
+}
+ 
+ 
+     
+   
+      
+         
+       
+
+
 
 
  
+
+ 
+    
+   
+
+
  
      
    

1d98856cb76144c9af301f758bc2a805b94c8189
La Fronterisima <159441596+lafronterisima@users.noreply.github.com>
2026-03-17 21:25:04 -0500

    Add files via upload
    Signed-off-by: La Fronterisima <159441596+lafronterisima@users.noreply.github.com>

--- js/vote.js
+++ js/vote.js
@@ -1,138 +1,130 @@
+const api ="https://script.google.com/macros/s/AKfycbwj3aBcHzOnOK3iAg_oSfRpq2qELyZg4CUeYMfK74vZrdxLjA7CcPy3NTBN6g8U-yh6Ow/exec";
 
-const api="https://script.google.com/macros/s/AKfycbwj3aBcHzOnOK3iAg_oSfRpq2qELyZg4CUeYMfK74vZrdxLjA7CcPy3NTBN6g8U-yh6Ow/exec";
+/* CONTROL */
+let cargando = false;
+let ultimaVersion = 0;
 
 /* CARGAR VOTOS */
-
 function cargarArtistas(){
+  if(cargando) return;
+  cargando = true;
 
-fetch(api)
-.then(res=>res.json())
-.then(data=>{
+  fetch(api + "?version=" + ultimaVersion)
+    .then(res => res.json())
+    .then(data => {
 
-data.sort((a,b)=>b.votes-a.votes);
+      // Si no hay cambios, no hace nada
+      if(!data || !data.artistas) return;
 
-data.forEach(a=>{
+      // Guardar versión
+      if(data.version){
+        ultimaVersion = data.version;
+      }
 
-let contador=document.getElementById("vote-"+a.id);
+      data.artistas.sort((a,b)=>b.votes-a.votes);
 
-if(contador){
-animarVotos(contador,a.votes);
+      data.artistas.forEach(a=>{
+        let contador = document.getElementById("vote-" + a.id);
+
+        if(contador){
+          let actual = parseInt(contador.innerText) || 0;
+
+          // Solo animar si cambia
+          if(actual !== a.votes){
+            animarVotos(contador, a.votes);
+          }
+        }
+      });
+
+    })
+    .catch(err => console.error(err))
+    .finally(()=>{ cargando = false; });
 }
 
-});
+/* ANIMACION SUAVE */
+function animarVotos(elemento, valorFinal){
+  let actual = parseInt(elemento.innerText) || 0;
 
-});
+  function frame(){
+    if(actual < valorFinal){
+      actual++;
+      elemento.innerText = actual + " votos";
+      requestAnimationFrame(frame);
+    } else {
+      elemento.innerText = valorFinal + " votos";
+    }
+  }
 
-}
-
-/* ANIMACION RAPIDA */
-
-function animarVotos(elemento,valorFinal){
-
-let actual=parseInt(elemento.innerText) || 0;
-
-let incremento=Math.ceil((valorFinal-actual)/10);
-
-let intervalo=setInterval(()=>{
-
-actual+=incremento;
-
-if(actual>=valorFinal){
-
-actual=valorFinal;
-clearInterval(intervalo);
-
-}
-
-elemento.innerText=actual+" votos";
-
-},30);
-
+  frame();
 }
 
 /* BOTONES */
-
 document.querySelectorAll(".vote-button").forEach(btn=>{
-
-btn.addEventListener("click",function(){
-
-let id=this.dataset.artist;
-votar(id);
-
+  btn.addEventListener("click", function(){
+    let id = this.dataset.artist;
+    votar(id);
+  });
 });
-
-});
-
 
 /* VOTAR */
-
 function votar(id){
+  if(localStorage.getItem("vote")){
+    mostrarMensaje("Ya votaste");
+    return;
+  }
 
-if(localStorage.getItem("vote")){
-mostrarMensaje("Ya votaste");
-return;
-}
+  fetch("https://api.ipify.org?format=json")
+    .then(res=>res.json())
+    .then(ip=>{
+      return fetch(api,{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/x-www-form-urlencoded"
+        },
+        body:"id=" + id + "&ip=" + ip.ip
+      });
+    })
+    .then(res=>res.text())
+    .then(data=>{
 
-fetch("https://api.ipify.org?format=json")
-.then(res=>res.json())
-.then(ip=>{
+      if(data === "already voted"){
+        mostrarMensaje("Ya votaste");
+      } else {
+        localStorage.setItem("vote","true");
 
-return fetch(api,{
-method:"POST",
-headers:{
-"Content-Type":"application/x-www-form-urlencoded"
-},
-body:"id="+id+"&ip="+ip.ip
-});
+        // ⚡ actualización inmediata
+        cargarArtistas();
 
-})
-.then(res=>res.text())
-.then(data=>{
+        mostrarMensaje("Voto registrado");
+      }
 
-if(data=="already voted"){
-
-mostrarMensaje("Ya votaste");
-
-}else{
-
-localStorage.setItem("vote","true");
-mostrarMensaje("Voto registrado");
-cargarArtistas();
-
-}
-
-})
-.catch(err=>{
-mostrarMensaje("Error al votar");
-console.error(err);
-});
-
+    })
+    .catch(err=>{
+      mostrarMensaje("Error al votar");
+      console.error(err);
+    });
 }
 
 /* MENSAJES */
-
 function mostrarMensaje(msg){
+  let box = document.getElementById("mensaje");
 
-let box=document.getElementById("mensaje");
+  if(box){
+    box.innerText = msg;
+    box.style.display = "block";
 
-if(box){
-
-box.innerText=msg;
-box.style.display="block";
-
-setTimeout(()=>{
-box.style.display="none";
-},3000);
-
+    setTimeout(()=>{
+      box.style.display = "none";
+    },3000);
+  }
 }
 
-}
-
-
-/* CARGAR AL ABRIR */
-
+/* INICIAR */
 cargarArtistas();
 
-/* ACTUALIZAR CADA 15 SEGUNDOS */
-
-setInterval(cargarArtistas,15000);
+/* AUTO-UPDATE INTELIGENTE */
+setInterval(()=>{
+  if(!document.hidden){
+    cargarArtistas();
+  }
+}, 2000);
